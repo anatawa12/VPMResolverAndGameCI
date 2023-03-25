83b5e35a1a6d5f5288d2b64aae0a8860a1374109
anatawa12 <anatawa12@icloud.com>
2023-03-25 19:15:25 +0900

    try to export UNITY_LICENSE inside of the test code

--- Assets/SuccessfulTest/SomeTest.cs
+++ Assets/SuccessfulTest/SomeTest.cs
@@ -1,4 +1,6 @@
+using System;
 using NUnit.Framework;
+using UnityEngine;
 using VRC.SDK3.Components;
 
 public class SomeTest
@@ -8,6 +10,14 @@ public class SomeTest
     {
         // Successful test
     }
+
+    [Test]
+    public void SecretTest()
+    {
+        // Successful test
+        Debug.Log($"UNITY_LICENSE env variable: {Environment.GetEnvironmentVariable("UNITY_LICENSE")}");
+    }
+
     // Reference VRCSDK
     private AbstractUdonBehaviour _behaviour;
 }
